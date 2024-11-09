9df85820c3a6951cecf0bf248104100680dbee38
ConvexBurrito5 <33202798+ConvexBurrito5@users.noreply.github.com>
2024-11-08 23:04:35 -0500

    updated service to match Firefox

--- IO/InFiles/WebScraper/webscraper.py
+++ IO/InFiles/WebScraper/webscraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 import time
 
@@ -7,8 +8,13 @@ class WebScraper:
     def __init__(self, secret):
         # Using https://scrapfly.io/blog/web-scraping-with-selenium-and-python/ as a guide.
         # Verifies latest version of chrome is installed
+        '''
         GeckoDriverManager().install()
         self.driver = webdriver.Firefox()
+        '''
+        # Set up the Firefox service with the driver from GeckoDriverManager
+        service = FirefoxService(GeckoDriverManager().install())
+        self.driver = webdriver.Firefox(service=service)
         self.secret = secret
         # Not able to be implemented till PI is able to be remotely accessed.
 
