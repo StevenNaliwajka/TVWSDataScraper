89a641f7bb22a12d65e33d26f6d4bd7f217de9ca
ConvexBurrito5 <33202798+ConvexBurrito5@users.noreply.github.com>
2024-11-08 23:17:50 -0500

    General Firefox driver getting, os exclusive

--- IO/InFiles/WebScraper/webscraper.py
+++ IO/InFiles/WebScraper/webscraper.py
@@ -1,3 +1,4 @@
+import platform
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -13,12 +14,26 @@ class WebScraper:
         self.driver = webdriver.Firefox()
         '''
         # Set up the Firefox service with the driver from GeckoDriverManager
-        service = FirefoxService(GeckoDriverManager().install())
-        self.driver = webdriver.Firefox(service=service)
+        self.driver = self.get_firefox_driver()
         self.secret = secret
         # Not able to be implemented till PI is able to be remotely accessed.
 
         self.driver.get("https://github.com/")
         time.sleep(500)
         self.driver.quit()
+
+    def get_firefox_driver(self):
+        system_architecture = platform.machine()
+
+        # For x86_64 architecture (your main PC)
+        if system_architecture == "x86_64":
+            service = FirefoxService(GeckoDriverManager().install())
+        # For ARM architectures (Raspberry Pi)
+        elif system_architecture in ["armv7l", "aarch64"]:
+            service = FirefoxService("/usr/local/bin/geckodriver")  # Path to manually installed geckodriver on Pi
+        else:
+            raise RuntimeError("Unsupported architecture: " + system_architecture)
+
+        # Initialize the WebDriver with the appropriate service
+        return webdriver.Firefox(service=service)
 
