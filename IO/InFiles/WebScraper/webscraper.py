a3b59dc3e7068f16566f89619cc5b3baf02281ee
ConvexBurrito5 <33202798+ConvexBurrito5@users.noreply.github.com>
2024-11-08 22:51:48 -0500

    changed to firefox

--- IO/InFiles/WebScraper/webscraper.py
+++ IO/InFiles/WebScraper/webscraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 import time
 
 
@@ -6,11 +7,12 @@ class WebScraper:
     def __init__(self, secret):
         # Using https://scrapfly.io/blog/web-scraping-with-selenium-and-python/ as a guide.
         # Verifies latest version of chrome is installed
-        self.driver = webdriver.Chrome()
+        GeckoDriverManager().install()
+        self.driver = webdriver.Firefox()
         self.secret = secret
         # Not able to be implemented till PI is able to be remotely accessed.
-        '''
-        driver.get("https://github.com/")
+
+        self.driver.get("https://github.com/")
         time.sleep(500)
-        driver.quit()
-        '''
+        self.driver.quit()
+
