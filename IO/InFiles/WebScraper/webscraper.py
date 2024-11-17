a7e81b4e1b9b2cdc7a6d4c42c1d236352bbb0b7c
ConvexBurrito5 <ConvexBurrito5@gmail.com>
2024-11-17 17:17:41 -0500

    added wait policy

--- IO/InFiles/WebScraper/webscraper.py
+++ IO/InFiles/WebScraper/webscraper.py
@@ -184,7 +184,9 @@ class WebScraper:
 
             # Gen Down DATA ID
             down_id = f"staConf{radio_count + 1}snr-value"
-            down_text = self.driver.find_element(By.ID, down_id).text
+            down_text = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, down_id))
+            ).text
             match = re.search(r"\[ (-?\d+) \| (-?\d+) \] (-?\d+) / (-?\d+) / (-?\d+)", down_text)
             if match:
                 radio.push_data("down_s0", int(match.group(1)))
