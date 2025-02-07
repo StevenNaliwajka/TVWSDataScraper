56553f508a9fb9ebed6cde215455c106d91fac0f
unknown <ConvexBurrito5@gmail.com>
2025-02-07 10:04:12 -0500

    l

--- minimal_debug.py
+++ minimal_debug.py
@@ -4,11 +4,12 @@ from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
 options = Options()
-options.add_argument("--headless")  # Remove if you need UI
+# options.add_argument("--headless")  # REMOVE HEADLESS MODE FOR DEBUGGING
 
 service = Service(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service, options=options)
 
 driver.get("https://www.google.com")
 print(driver.title)
+
 driver.quit()
