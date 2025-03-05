390ec058f4cd4d4c4837ff2077dd81783d25d747
unknown <ConvexBurrito5@gmail.com>
2025-03-05 15:50:20 -0500

    error catching

--- CodeBase/Webscraper/Navigate/open_all_radio_menus.py
+++ CodeBase/Webscraper/Navigate/open_all_radio_menus.py
@@ -1,18 +1,26 @@
-from selenium.common import NoSuchElementException
+import time
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+def open_all_radio_menus(driver):
 
-def open_all_radio_menus(driver,):
     button_number = 1
 
     while True:
         button_id = f"sta{button_number}btn"
         try:
-            # Check for button existence
-            button = driver.find_element(By.ID, button_id)
-            # Use JavaScript to click the button
+            # Wait up to 5 seconds for the button to be present
+            button = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.ID, button_id))
+            )
+            # Click using JavaScript
             driver.execute_script("arguments[0].click();", button)
             button_number += 1
         except NoSuchElementException:
             # Stop the method when button is not found
+            break
+        except Exception as e:
+            print(f"Error encountered: {e}")
             break
