0fe51eb30c58ef05a5040ff576a406c3bcea6d53
unknown <ConvexBurrito5@gmail.com>
2025-03-05 16:19:20 -0500

    update

--- CodeBase/Webscraper/ReadDataFromWeb/DownLinkTable/read_ear_time_column.py
+++ CodeBase/Webscraper/ReadDataFromWeb/DownLinkTable/read_ear_time_column.py
@@ -1,10 +1,29 @@
+import re
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 def read_ear_time_column(driver, radio_count, radio):
-    ear_time_id = f"staConf{radio_count + 1}temp-value"
-    radio_uptime = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, ear_time_id))
-    ).text
-    radio.radio_uptime = radio_uptime
+    # Get base station uptime text
+    ear_uptime_value = driver.find_element(By.ID, f"staConf{radio_count + 1}uptime-value").text
+
+    # extract
+    matches = re.findall(r"(\d+)Days|(\d+)h|(\d+)m|(\d+)s", ear_uptime_value)
+
+    days, hours, minutes, seconds = 0, 0, 0, 0
+
+    for match in matches:
+        if match[0]:
+            days = int(match[0])
+        if match[1]:
+            hours = int(match[1])
+        if match[2]:
+            minutes = int(match[2])
+        if match[3]:
+            seconds = int(match[3])
+
+    # Format
+    formatted_uptime = f"{days}-{hours:02}:{minutes:02}:{seconds:02}"
+    print(f"(DEBUG) Uptime: {formatted_uptime}")
+    radio.uptime_value = formatted_uptime
