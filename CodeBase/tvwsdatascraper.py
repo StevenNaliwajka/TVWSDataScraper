b3bc2b3c3e3d91734bce0b1e1def5c5a04f96333
unknown <ConvexBurrito5@gmail.com>
2025-03-05 15:33:13 -0500

    update

--- CodeBase/tvwsdatascraper.py
+++ CodeBase/tvwsdatascraper.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+# Dynamically find the project root, required to allow for correct imports.
+folder_ct = 1  ### NOT always 2. Change to the qty of folders up before root.
+PROJECT_ROOT = Path(__file__).resolve().parents[folder_ct]
+sys.path.append(str(PROJECT_ROOT))
+
 from CodeBase.ConfigManager.config import Config
 from CodeBase.ConfigManager.ConfigRead.read_outfile_config import read_outfile_config
 from CodeBase.ConfigManager.ConfigRead.read_radio_config import read_radio_config
